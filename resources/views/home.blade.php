6b2f17091b87811431f2dd7a28045d89bb60af8b
stefanenache12 <stefanenache12@gmail.com>
2023-09-08 17:50:12 +0200

    ADD COMICS ON THE MAIN PAGE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,16 @@
 @section('page-title', 'Homepage')
 
 @section('main-content')
-    {{-- @foreach ($comics as $InnerComics)
-        <ul>
-            @foreach ($InnerComics as $comic)
-                <li>{{ $comic }}</li>
+    <div class="jumbotron">
+    </div>
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-3 text-center">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <h5 class="p-4">{{ $comic['title'] }}</h5>
+                </div>
             @endforeach
-        </ul>
-    @endforeach --}}
+        </div>
+    </div>
 @endsection
